d9fad5e309d06a59e9177c4e0b858961f67fe51e
MadRex2000 <spplkk113@gmail.com>
2020-06-16 12:14:03 +0800

    update fishing status report form date/time picker

--- fishingMap/map/forms.py
+++ fishingMap/map/forms.py
@@ -6,3 +6,7 @@ class fishingStatusForm(forms.ModelForm):
     class Meta:
         model = fishingStatus
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['time'].widget.attrs.update({'id': 'id_pub_date'})
